bfdb85b0235dd9d8c2beb495ba634fae6dd8bece
Darryl Ng <darryl1975@hotmail.com>
2023-02-08 12:32:14 +0800

    Add Employee part 1

--- src/main/java/sg/edu/nus/iss/day13_lecture/controller/EmployeeController.java
+++ src/main/java/sg/edu/nus/iss/day13_lecture/controller/EmployeeController.java
@@ -1,5 +1,35 @@
 package sg.edu.nus.iss.day13_lecture.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import java.util.*;
+import sg.edu.nus.iss.day13_lecture.repository.EmployeeRepo;
+import sg.edu.nus.iss.day13_lecture.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
     
+    @Autowired
+    EmployeeRepo empRepo;
+
+    @GetMapping("/home")
+    public String employeeListPage(Model model) {
+        List<Employee> employees = empRepo.findAll();
+        model.addAttribute("employees", employees);
+
+        return "employeeslist";
+    }
+
+    @GetMapping("/addnew")
+    public String addPage(Model model) {
+        Employee emp = new Employee();
+        model.addAttribute("employee", emp);
+        return "employeeadd";
+    }
+
 }
